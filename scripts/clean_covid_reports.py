7aeb14bb0275ccae930e8ef3c9a9250b2ce01b32
bailez <bailez@usp.br>
2021-09-06 18:20:29 -0300

    add data and some scripts

---
+++ scripts/clean_covid_reports.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+from tika import parser
+import os
+
+COLUMNS = ['Casos Suspeitos', 'Casos Confirmados', 'Descartados', 'Infectados',
+           'Cura Clinica', 'Obitos']
+
+INDEX = ['Alagoas e Sergipe', 'Altamira', 'Alto Rio Juruá', 'Alto Rio Negro', 
+         'Alto Rio Purus', 'Alto Rio Solimões', 'Amapá e Norte do Pará',
+         'Araguaia', 'Bahia', 'Ceará', 'Cuiabá', 'Guamá-Tocantins',
+         'Interior Sul', 'Kaiapó do Mato Grosso', 'Kaiapó do Pará', 'Leste de Roraima',
+         'Litoral Sul', 'Manaus', 'Maranhão', 'Mato Grosso do Sul', 'Médio Rio Purus',
+         'Médio Rio Solimões e Afluentes', 'Minas Gerais e Espírito Santo',
+         'Parintins', 'Pernambuco', 'Porto Velho', 'Potiguara', 'Rio Tapajós',
+         'Tocantins', 'Vale do Javari', 'Vilhena', 'Xavante', 'Xingu','Yanomami']
+
+
+def get_raw_text(file : str) -> str:
+    
+    text = parser.from_file(file)['content']
+    
+    return text
+
+def generate_table(text : str, index : list) -> list:
+
+    values = text[text.find('alagoas') : text.find('total')].lower()
+    
+    for i in index:
+        values = values.replace(i.lower(), '')
+    
+    values = values.split('\n')
+    
+    values = list(filter(lambda x: len(x) > 0, values))
+    
+    values = list(map(lambda x: x.split(' '), values))
+    
+    table = []
+    
+    for col in values:
+        column = list(filter(lambda x: x != '', col))
+        table.append(column)
+  
+    return table
+
+def create_panel(table : list, 
+                 index : list, 
+                 columns : list) -> pd.DataFrame:
+    
+
+    df = pd.DataFrame(table)
+
+    df.index = index    
+
+    df.index.name = 'DSEI'
+
+    df.columns = columns
+
+    df = df.astype(int)
+    
+    return df
+
+## == ## == ## == ## == ## == ## == ## == ## == ## == ## == ## == ## == ##
+
+all_reports = os.listdir(r'data/RAW/MS')
+
+for report in all_reports:
+    
+    print(report)
+            
+    text = get_raw_text(file = r'data/RAW/MS/' + report).lower()
+    
+    table = generate_table(text = text, index = INDEX)
+    
+    try:
+    
+        df = create_panel(table = table, index = INDEX, columns = COLUMNS)
+    
+    except Exception as e:
+        print(e)
+        
+    
+    date = report[7:-4]
+    
+    date = pd.to_datetime(date, format= '%d-%m-%Y')
+    
+    df['Data'] = date
+            
+    df.to_csv(r'data/cleaned/MS/' + report[:-4] + '.csv')
+
---
+++ scripts/download_covid_reports.py
@@ -0,0 +1,21 @@
+import requests as req
+import pandas as pd
+
+dates = pd.date_range(start='01-01-2020', end='12-31-2021')
+
+
+for date in dates:
+    current_date = date.strftime('%d-%m-%Y')
+    
+    url = 'https://saudeindigena1.websiteseguro.com/coronavirus/pdf/'\
+        f'{current_date}_Boletim epidemiologico SESAI sobre COVID 19.pdf'
+        
+    
+    response = req.get(url)
+    
+    if response.status_code != 200:
+        continue
+
+    with open(f'report-{current_date}.pdf', 'wb') as f:
+        f.write(response.content)
+    
