e2db3496a12f27e23285c614e168f093d05710f9
bailez <bailez@usp.br>
2021-11-11 12:22:37 -0300

    add plot

--- scripts/atividade_7.py
+++ scripts/atividade_7.py
@@ -10,9 +10,7 @@ from statsmodels.tsa.stattools import adfuller, kpss
 
 
 os.chdir(r'C:\Users\felip\OneDrive\Documentos\FEA\Econometria 3')
-
-
-
+'''
 all_reports = os.listdir(r'data/cleaned/inpe/')[:-1]
 satelite = 'AQUA_M-T'
 bdq = pd.read_csv(r'data/cleaned/inpe/bdq_AQUA.csv')
@@ -29,82 +27,150 @@ biomas = bdq.bioma.drop_duplicates().dropna().values
 for b in biomas:
     bdq_b = bdq[bdq['bioma'] == b]
     df[b] = bdq_b['N'].resample(freq).sum().fillna(0)
-
-
+'''
+df = pd.read_excel(r'amazon-fire-covid\data.xlsx')
+df = df.set_index('Data')
 biomas = ['Cerrado', 'Mata Atlantica', 'Amazonia']
 
 df = df[biomas]
-df.to_excel(r'C:\Users\felip\OneDrive\Documentos\FEA\Econometria 3\amazon-fire-covid\data.xlsx')
+df = np.log(df)
+#df.to_excel(r'C:\Users\felip\OneDrive\Documentos\FEA\Econometria 3\amazon-fire-covid\data.xlsx')
 cerrado = np.log(df['Cerrado'])
 mata_atlantica = np.log(df['Mata Atlantica'])
 amazonia = np.log(df['Amazonia'])
 # %%
+decomp = {}
 
-cerrado_decomp = seasonal_decompose(cerrado, model="additive")
-matatl_decomp = seasonal_decompose(mata_atlantica, model="additive")
-amazonia_decomp = seasonal_decompose(amazonia, model="additive")
+for i in biomas:
+    decomp.update({i: seasonal_decompose(df[i])})
 # %%
+'''
+Plot seasonal decomp
+'''
+trends = {}
+for i in decomp:    
+    plot_seasonalcomp(df[i],decomp[i])
+    trends.update({i : decomp[i].trend.dropna()})
 
-plot_seasonalcomp(cerrado,cerrado_decomp)
-plot_seasonalcomp(mata_atlantica,matatl_decomp)
-plot_seasonalcomp(amazonia,amazonia_decomp)
 # %% descrição
 '''
 Variable description
 '''
+for i in biomas:
+    print('\n\n', i,'\n')
+    print(trends[i].describe().to_latex())
 
-print(df.describe().to_latex())
-
-
+fig, axs = plt.subplots(3,1,figsize=(8,12))
+c= 0
+for i in biomas:
+    trends[i].plot(ax=axs[c], legend=True, label=i)
+    c += 1
 # %%
 
 '''
 
 teste ADF
+
+'''
+adfs = []
+
+for i in biomas:
+    adf = adfuller(trends[i])
+
+    adf_df = pd.Series(adf[:3], index = [(i,'adf'), 
+                                     (i,'pvalue'),
+                                     (i,'lag')])
+    adf_df.index = pd.MultiIndex.from_tuples(adf_df.index)
+    adfs.append(adf_df)
+    
+adfs = pd.DataFrame(pd.concat(adfs)).T
+print(adfs.to_latex())
+
+# %%
+
 '''
 
-adf = adfuller(df)
+ KPSS
+ 
+'''
+kpss_list = []
 
-adf_df = pd.Series(adf[:3], index = [('original','adf'), 
-                                     ('original','pvalue'),
-                                     ('original','lag')])
-adf_df.index = pd.MultiIndex.from_tuples(adf_df.index)
+for i in biomas:
+    kpss_test = kpss(trends[i])
+
+    kpss_df = pd.Series(kpss_test[:3], index = [(i,'adf'), 
+                                     (i,'pvalue'),
+                                     (i,'lag')])
+    
+    kpss_df.index = pd.MultiIndex.from_tuples(kpss_df.index)
+    kpss_list.append(kpss_df)
+    
+kpss_list = pd.DataFrame(pd.concat(kpss_list)).T
+print(kpss_list.to_latex())
+
+
+# %%
+
+
+'''
+Variable description first differences
+'''
+trends_fd = {}
+for i in decomp:    
+    plot_seasonalcomp(df[i],decomp[i])
+    trends_fd.update({i : decomp[i].trend.diff().dropna()})
+
+for i in biomas:
+    print('\n\n', i,'\n')
+    print(trends_fd[i].describe().to_latex())
+
+fig, axs = plt.subplots(3,1,figsize=(8,12))
+c= 0
+for i in biomas:
+    trends_fd[i].plot(ax=axs[c], legend=True, label=i)
+    c += 1
 # %%
 
 
 '''
 
-Melhor modelo Enders
+teste ADF first_differences
 
 '''
+adfs = []
 
-# %%
+for i in biomas:
+    adf = adfuller(trends_fd[i])
 
-
-'''
-
- ADF-GLS, PP e KPSS
-'''
-
-adf = kpss(df)
-
-adf_df = pd.Series(adf[:3], index = [('original','adf'), 
-                                     ('original','pvalue'),
-                                     ('original','lag')])
-adf_df.index = pd.MultiIndex.from_tuples(adf_df.index)
-
-# %%
-
-'''
-Teste em primeira diferença
-
-'''
+    adf_df = pd.Series(adf[:3], index = [(i,'adf'), 
+                                     (i,'pvalue'),
+                                     (i,'lag')])
+    adf_df.index = pd.MultiIndex.from_tuples(adf_df.index)
+    adfs.append(adf_df)
+    
+adfs = pd.DataFrame(pd.concat(adfs)).T
+print(adfs.to_latex())
 
 
 # %%
 
+'''
+
+teste KPSS first_differences
 
 '''
-Grau de integração das séries
 
-'''
+kpss_list = []
+
+for i in biomas:
+    kpss_test = kpss(trends_fd[i])
+
+    kpss_df = pd.Series(kpss_test[:3], index = [(i,'adf'), 
+                                     (i,'pvalue'),
+                                     (i,'lag')])
+    
+    kpss_df.index = pd.MultiIndex.from_tuples(kpss_df.index)
+    kpss_list.append(kpss_df)
+    
+kpss_list = pd.DataFrame(pd.concat(kpss_list)).T
+print(kpss_list.to_latex())
